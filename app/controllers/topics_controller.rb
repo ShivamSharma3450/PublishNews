38c2af35bb773308bdd2ac6ff4ca12ba798d1c5a
Shubham Mishra <ramsmishra111@gmail.com>
2020-09-18 00:34:55 +0530

    LogIn SignUp animated screen, next will be user module with auth and everything with Gems and then reply section on news with @ user tagged

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -64,6 +64,10 @@ class TopicsController < ApplicationController
     end
   end
 
+  def login
+    render 'login'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'topics#index'
   resources :topics
   post '/upload/images', to: "info#upload_image"
+  get 'login', to: "topics#login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
